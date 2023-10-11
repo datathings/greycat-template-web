e4398faf9ab38a12ef10e4429ed52834b1da43e1
Maxime Tricoire <maxime.tricoire@datathings.com>
2023-10-11 17:30:53 +0200

    \o/

--- build.sh
+++ build.sh
@@ -8,14 +8,14 @@ GREYCAT_LANG_VERSION=${4:-"6.3.5-dev"}
 
 # install greycat
 curl -qsL https://get.greycat.io/files/core/${GREYCAT_CORE_BRANCH}/x86-linux/${GREYCAT_CORE_MAJOR}/${GREYCAT_CORE_VERSION}.zip -o tmp.zip &&
-  unzip -d greycat -oqq tmp.zip &&
+  unzip -d /tmp/greycat -oqq tmp.zip &&
   rm tmp.zip
 # install greycat-lang
 curl -qsL https://get.greycat.io/files/lang/${GREYCAT_CORE_BRANCH}/${GREYCAT_CORE_MAJOR}/${GREYCAT_LANG_VERSION}.zip -o tmp.zip &&
   unzip -d /tmp/greycat -oqq tmp.zip &&
   rm tmp.zip
-GREYCAT_HOME="/tmp/greycat"
-PATH="$PATH:$GREYCAT_HOME/bin"
+export GREYCAT_HOME="/tmp/greycat"
+export PATH="$PATH:$GREYCAT_HOME/bin"
 
 pnpm install
 pnpm lint
