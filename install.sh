8115784166782cdbec39736198b1c4471c2dc930
François Fouquet <francois.fouquet@datathings.com>
2023-10-18 17:19:16 +0200

    new version 6.4

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 set -e
 GREYCAT_DIR=${GREYCAT_HOME:-"$HOME/.greycat"}
-GREYCAT_ARCH="x86"
+GREYCAT_ARCH="x64"
 if [[ $(uname -m) == 'arm64' ]]; then
-    GREYCAT_ARCH="arm"
+    GREYCAT_ARCH="arm64"
 fi
 GREYCAT_OS="linux"
 if [[ $(uname) == 'Darwin' ]]; then
     GREYCAT_OS="apple"
 fi
 GREYCAT_TARGET="${GREYCAT_ARCH}-${GREYCAT_OS}"
-GREYCAT_CORE_VERSION="dev/${GREYCAT_TARGET}/6.3/6.3.86-dev"
-GREYCAT_LANG_VERSION="dev/6.3/6.3.5-dev"
-GREYCAT_EXPLORER_VERSION="dev/6.3/6.3.33-dev"
-curl -qsL https://get.greycat.io/files/core/${GREYCAT_CORE_VERSION}.zip -o tmp.zip && unzip -d ${GREYCAT_DIR} -oqq tmp.zip && rm tmp.zip
-curl -qsL https://get.greycat.io/files/lang/${GREYCAT_LANG_VERSION}.zip -o tmp.zip && unzip -d ${GREYCAT_DIR} -oqq tmp.zip && rm tmp.zip
-curl -qsL https://get.greycat.io/files/apps/explorer/${GREYCAT_EXPLORER_VERSION}.zip -o tmp.zip && unzip -d "$GREYCAT_DIR" -oqq tmp.zip && rm tmp.zip
+GREYCAT_BRANCH="dev"
+GREYCAT_CORE_VERSION="6.4/6.28-dev"
+GREYCAT_LANG_VERSION="6.4/6.4.3-dev"
+GREYCAT_EXPLORER_VERSION="6.4/6.4.6-dev"
+GREYCAT_CORE_VERSION_ARCH=$(printf "${GREYCAT_CORE_VERSION}" | sed "s/\//\/${GREYCAT_TARGET}\//")
+curl -qsL https://get.greycat.io/files/core/${GREYCAT_BRANCH}/${GREYCAT_CORE_VERSION_ARCH}.zip -o tmp.zip && unzip -d ${GREYCAT_DIR} -oqq tmp.zip && rm tmp.zip
+curl -qsL https://get.greycat.io/files/lang/${GREYCAT_BRANCH}/${GREYCAT_LANG_VERSION}.zip -o tmp.zip && unzip -d ${GREYCAT_DIR} -oqq tmp.zip && rm tmp.zip
+curl -qsL https://get.greycat.io/files/apps/explorer/${GREYCAT_BRANCH}/${GREYCAT_EXPLORER_VERSION}.zip -o tmp.zip && unzip -d "$GREYCAT_DIR" -oqq tmp.zip && rm tmp.zip
